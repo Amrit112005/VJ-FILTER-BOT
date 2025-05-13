f23fa59427feb5ac9a39fcd2e7f5c578e502bd54
Amrit112005 <amrit2005pay@gmail.com>
2025-05-13 14:50:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -250,7 +250,6 @@ Nᴀᴍᴇ - {}"""
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """<a href='https://t.me/rudraraut'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
-    Powerd by: @rudraraut
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
